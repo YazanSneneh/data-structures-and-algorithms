513740c2a8dd945b0f897d98d3328333a1b5d4ef
YazanSneneh <yaz.sneneh@gmail.com>
2021-05-22 14:07:53 +0300

    finish resubmissions

--- challenges/selectionsort/SelectionSortTest.java
+++ challenges/selectionsort/SelectionSortTest.java
@@ -9,12 +9,36 @@ public class SelectionSortTest {
     @Test
     public void edgeCaseTest() {
         Integer[] numbers = new Integer[0];
-           assertEquals("the result should be empty", "",insertionSort(numbers));
+        Integer[] test = {};
+           assertArrayEquals( test ,insertionSort(numbers));
         }
 
     @Test
     public void happyPath() {
        Integer[] numbers = {8,4,23,42,16,15};
-           assertEquals( "the result should be", "4 8 15 16 23 42 ", insertionSort(numbers));
+       Integer[] sortedResult = {4,8, 15, 16, 23,42 };
+        assertArrayEquals(  sortedResult, insertionSort(numbers));
+    }
+
+//    ...........................
+    @Test
+    public void reverseSorted() {
+     Integer[] numbers = {8,4,23,42,16,15};
+        Integer[] sortedResult = {4,8, 15, 16, 23,42 };
+        assertArrayEquals(  sortedResult, insertionSort(numbers));
+    }
+
+    @Test
+    public void nearlySorted() {
+        Integer[] numbers = {8,4,23,42,16,15};
+        Integer[] sortedResult = {4,8, 15, 16, 23,42 };
+        assertArrayEquals( sortedResult, insertionSort(numbers));
+    }
+
+    @Test
+    public void fewUniques() {
+        Integer[] numbers = {8,4,23,42,16,15};
+        Integer[] sortedResult = {4,8, 15, 16, 23,42 };
+        assertArrayEquals(  sortedResult, insertionSort(numbers));
     }
 }
